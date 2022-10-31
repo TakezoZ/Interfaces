01e9991cd3ca85364e22dd0c2f93a113607ebab6
Marcos Takezo Otta <Takezootta02@gmail.com>
2022-10-31 11:18:50 -0300

    Exemplo de interfaçe V1(Resolvendo sem o uso de Interface)

---
+++ BasicInterface/BasicInterface/Program.cs
@@ -0,0 +1,36 @@
+﻿using BasicInterface.Entities;
+using BasicInterface.Services;
+using System.Globalization;
+
+namespace BasicInterface
+{
+    class Program
+    {
+        static void Main(string[] Args)
+        {
+
+            Console.WriteLine("Enter rental data");
+            Console.Write("Car model: ");
+            string carModel = Console.ReadLine();
+            Console.Write("Pickup (dd/MM/yyyy hh:ss): ");
+            DateTime pickUpDate = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:ss", CultureInfo.InvariantCulture);
+            Console.Write("Return (dd/MM/yyyy hh:ss): ");
+            DateTime returnDate = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:ss", CultureInfo.InvariantCulture);
+
+            Console.Write("Enter price per hour: ");
+            double pricePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter price per day: ");
+            double pricePerDay = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(pickUpDate, returnDate, new Vehicle(carModel));
+
+            RentalService rentalService = new RentalService(pricePerHour, pricePerDay);
+
+            rentalService.ProcessInvoice(carRental);
+
+            Console.WriteLine();
+            Console.WriteLine("INVOICE: ");
+            Console.WriteLine(carRental.Invoice);
+        }
+    }
+}
---
+++ BasicInterface/BasicInterface/Services/BrazilTaxService.cs
@@ -0,0 +1,17 @@
+﻿namespace BasicInterface.Services
+{
+    internal class BrazilTaxService
+    {
+        public double Tax(double amount)
+        {
+            if (amount <= 100)
+            {
+                return amount * 0.20;
+            }
+            else
+            {
+                return amount * 0.15;
+            }
+        }
+    }
+}
