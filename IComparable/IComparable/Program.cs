fea42a6d30bd6578245c6ea6d927bd73ada5d09d
Marcos Takezo Otta <Takezootta02@gmail.com>
2022-10-31 15:33:00 -0300

    Interface Icomparable-v1(Proposta1)

---
+++ IComparable/IComparable/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace IComparable
+{
+    class Program
+    {
+        static void Main(string[] Args)
+        {
+            string path = @"c:\temp\in.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<string> list = new List<string>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(sr.ReadLine());
+                    }
+                    list.Sort();
+                    foreach (string str in list)
+                    {
+                        Console.WriteLine(str);
+                    }
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
